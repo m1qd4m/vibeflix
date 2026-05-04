d52a6943e213e9c92eb05f5ad5f9f12d8009464a
m1qd4m <miqdam633@gmail.com>
2026-05-04 18:39:30 +0500

    Clean project and update gitignore

---
+++ lib/firebase_options.dart
@@ -0,0 +1,27 @@
+// GENERATED FILE - Replace with your own firebase_options.dart
+// Run: flutterfire configure
+// Install: dart pub global activate flutterfire_cli
+
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      default:
+        return android;
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyA7B-YQkWkxF4tQ1MRyFmdy8Ad69TguL6g',
+    appId: '1:64160679064:android:7e4fded74ac1ddbc6bbced',
+    messagingSenderId: '64160679064',
+    projectId: 'vibeflix-73f9e',
+    storageBucket: 'vibeflix-73f9e.firebasestorage.app',
+  );
+
+  // ⚠️  REPLACE ALL VALUES BELOW with your actual Firebase project config
+}
